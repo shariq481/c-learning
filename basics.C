1a1f9ef43b6360a5926aadc6923bc3a9c876bf37
shariq481 <syedshariq786786@gmail.com>
2026-08-27 21:20:35 +0530

    arithmetic operators

---
+++ basics.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+int a ,b;
+printf("enter your first number a");
+scanf("%d",&a);
+printf("enter your second number b");
+scanf("%d",&b);
+int sum=a +b;
+int subtraction=a-b;
+int product=a*b;
+int division=a/b;
+int remainder=a%b;
+printf("sum=%d\n,subtraction=%d\n,product=%d\n,division=%d\n,remainder=%d\n",sum,subtraction,product,division,remainder);
+return 0;
+
+
+}
+
