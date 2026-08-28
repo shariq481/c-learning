9b425dcbce691ae6ed377da8ab4e20f7da8d6703
shariq481 <syedshariq786786@gmail.com>
2026-08-28 22:09:04 +0530

    cube and square

---
+++ .vscode/cube.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    printf("enter number to find cube  :");
+    scanf("%d",&a);
+    printf("cube is %d\n",a*a*a);
+ int b;
+    printf("enter side of square");
+    scanf("%d",&b);
+    printf("area of square is :%d",b*b);
+    return 0;
+}
