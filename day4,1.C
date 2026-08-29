fd0f188855d7d0ef70e8ac0c6dfa79108413665c
shariq481 <syedshariq786786@gmail.com>
2026-08-29 22:29:44 +0530

    adult or not

---
+++ day4,1.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+    int age ;
+    printf("enter your age : ");
+    scanf("%d",&age);
+    if (age >18)
+    {
+        printf("you are an adult and can vote\n22 ");
+        printf("you are eligible for driving\n ");
+    }
+    else
+    {
+        printf("you are under age\n ");
+
+    }   
+        printf("thanks for your input");
+
+        return 0 ;
+
+
+}
