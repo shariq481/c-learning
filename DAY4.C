29dde6a8792113395ef4bf895d012712b393e9fe
shariq481 <syedshariq786786@gmail.com>
2026-08-29 22:07:48 +0530

    day4

---
+++ DAY4.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int height,base;
+printf("enter height of triangle :");
+scanf("%d",&height);
+printf("enter base of triangle:");
+scanf("%d",&base);
+printf("area of triangle is :%d",height*base*1/2);
+return 0;
+
+
+}
