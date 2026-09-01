56594bec66bb70fde4fa810381df0804bbb5f9ce
shariq481 <syedshariq786786@gmail.com>
2026-09-01 20:46:24 +0530

    even or odd

---
+++ even.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+        int i,j;
+        scanf("%d ",&i);
+        if (i % 2 == 0)
+        {
+                printf("Even\n");
+        }
+        else
+        {
+                printf("Odd\n");
+        }
+        printf("thanks for input");
+        return 0;
+}
