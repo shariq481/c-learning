cf39da70105fb0c468c6e7d72c82239a9c5f4d27
shariq481 <syedshariq786786@gmail.com>
2026-09-03 23:09:53 +0530

    star

---
+++ day8,1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{   int n;
+    printf("enter the number of rows");
+    scanf("%d",&n);
+
+    
+    for (int i =0;i<=n;i++)
+{
+        printf("*");
+
+
+
+}
+
+printf("\n");
+
+    
+return 0;
+
+
+
+}
