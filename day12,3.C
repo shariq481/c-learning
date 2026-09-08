b13d8e83a2d4b65101d18c20597d33c015f5c0b7
shariq481 <syedshariq786786@gmail.com>
2026-09-08 22:06:33 +0530

    electricity bill

---
+++ day12,3.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main ()
+{
+    int consumed;
+    printf("enter the amount of electricity consumed in units: ");
+    scanf("%d", &consumed);
+    if (consumed<0){
+        printf("you have entered an invalid number");
+    }
+     else if (consumed<=100)
+    {
+        printf("the bill is %d", consumed*2);
+    }
+  else if (consumed>100&&consumed<=200)
+  {
+    printf("the bill is %d ",3*consumed);
+  }
+    else if (consumed>200&&consumed<=400)
+    {
+    printf("the bill is %d",consumed*5);
+}
+    else if (consumed>400)
+    {
+        printf("the bill is %d",consumed*7);
+    }
+
+}
