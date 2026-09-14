306e367773508189aa3ca034a5485c9bd8cd2ac5
shariq481 <syedshariq786786@gmail.com>
2026-09-14 22:34:58 +0530

    type  of triangle

--- day15.C
+++ day15.C
@@ -1,3 +1,18 @@
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+
+int main() {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+
+    if (A <= 0 || B <= 0 || C <= 0 || A + B <= C || A + C <= B || B + C <= A) {
+        printf("Not a Triangle");
+    } else if (A == B && B == C) {
+        printf("Equilateral Triangle");
+    } else if (A == B || B == C || A == C) {
+        printf("Isosceles Triangle");
+    } else {
+        printf("Scalene Triangle");
+    }
+
+    return 0;
 }
