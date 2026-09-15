66920b598d1956d9e75e16f61dab436dc91bb03f
shariq481 <syedshariq786786@gmail.com>
2026-09-15 20:56:38 +0530

    switch

---
+++ day17.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    switch(n%2)
+    {
+        case 0:
+            printf("Even\n");
+            break;
+        default:
+            printf("Odd\n");
+    }
+}
