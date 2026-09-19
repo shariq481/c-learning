c16bcc886dd72d19983f8f8e22723e1778a9c68d
shariq481 <syedshariq786786@gmail.com>
2026-09-19 19:00:33 +0530

    switch

---
+++ switch.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main ()
+    {int a,b;char c;
+        scanf("%d %c %d",&a ,&c,&b);
+        switch(c) 
+        { 
+                case '+':
+                printf("%d",a+b);
+                break;
+                case '-':
+                printf("%d",a-b);
+                break;
+                case'*':
+                printf("%d",a*b);
+                break;
+                case'/':
+                printf("%d",a/b);
+                break;
+                case'%':
+                printf("%d",a%b);
+                break;
+                default:
+                printf("Invalid Operator");
+        }
+    return 0;
+    
+    
+    }
